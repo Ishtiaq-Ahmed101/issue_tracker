fedfc375a60da31ddff4aaeaa319cb55ef5d968c
Ishtiaq Ahmed <awais9027prince@gmail.com>
2023-10-17 19:56:07 +0500

    update

--- issue-tracker/app/NavBar.tsx
+++ issue-tracker/app/NavBar.tsx
@@ -1,8 +1,12 @@
+'use client';
 import React from 'react'
 import Link from 'next/link'
 import {AiFillBug} from 'react-icons/ai'
+import { usePathname } from 'next/navigation'
+import classNames from 'classnames';
 
 const NavBar = () => {
+    const currentPath = usePathname();
     const links = [
         {label:"Dashboard", href:'/'},
         {label: "Issues", href:'/issues'},
@@ -13,7 +17,11 @@ const NavBar = () => {
         <ul className='flex space-x-6'>
             {links.map(link=> <Link
              key={link.href}
-              className='text-zinc-500 hover:text-zinc-800 transition-colors' href={link.href}>{link.label}</Link> )}
+              className={classNames({
+                'text-zinc-900': link.href === currentPath,
+                'text-zinc-500': link.href !== currentPath,
+                'hover:text-zinc-800 transition-colors': true
+              })} href={link.href}>{link.label}</Link> )}
 
         </ul>
     </nav>
